6a094bc2464e21cc98f7bd4e5396539d18eddccb
mzahng160 <mzhang160@163.com>
2019-06-06 00:36:32 +0800

    update TimerQueue.h

--- TimerQueue.h
+++ TimerQueue.h
@@ -1,4 +1,15 @@
 #ifndef _TIMERQUEUE_H
 #define _TIMERQUEUE_H
 
+#include "Declear.h"
+#include "IChannelCallback.h"
+
+class TimerQueue : public IChannelCallback
+{
+public:
+	TimerQueue();
+	~TimerQueue();
+	
+};
+
 #endif
