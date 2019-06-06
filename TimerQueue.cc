f4adcb859d9d809683fae601706a1399bd2e0f70
root <root@localhost.localdomain>
2019-06-06 20:57:54 +0800

    update timerQueue

--- TimerQueue.cc
+++ TimerQueue.cc
@@ -1,1 +1,57 @@
 #include "TimerQueue.h"
+#include "Channel.h"
+
+#include <unistd.h>
+//#include <stdio.h>
+//#include <inttypes.h>
+#include <sys/timerfd.h>
+
+TimerQueue::TimerQueue(EventLoop* pLoop)
+    :_timerfd(createTimerId())
+    ,_pLoop(pLoop)
+    ,_timerfdChannel(new Channel(pLoop, _timerfd))
+    ,_addTimerWrapper(new AddTimerWrapper(this))
+    ,_cancelTimerWrapper(new CancelTimerWrapper(this))
+{
+	_timerfdChannel;
+}
+TimerQueue::~TimerQueue()
+{
+	::close(_timerfd);
+}
+
+void TimerQueue::doAddTimer(void* param)
+{
+	Timer* pTimer = static_cast<Timer*>(param);
+	bool earliestChanged = insert(pTimer);
+	if(earliestChanged)
+	{
+		resetTimerfd(_timerfd, pTimer->getStamp());
+	}
+}
+
+void TimerQueue::doCancelTimer(void* param)
+{
+
+}
+
+int TimerQueue::createTimerId()
+{
+	
+}
+
+void TimerQueue::resetTimerfd(int timerfd, Timestamp stamp)
+{
+	struct itimerspec newValue;
+}
+
+bool TimerQueue::insert(Timer* pItem)
+{
+	bool earliestChanged = false;
+	Timestamp when = pItem->getStamp();
+	TimerList::iterator it = _timers.begin();
+	if(it == _timers.end() || when < it->first)
+	{
+		earliestChanged = true;
+	}
+}
--- TimerQueue.h
+++ TimerQueue.h
@@ -3,13 +3,105 @@
 
 #include "Declear.h"
 #include "IChannelCallback.h"
+#include "IRun.h"
+#include "Timestamp.h"
+#include <set>
+#include <vector>
 
 class TimerQueue : public IChannelCallback
 {
 public:
-	TimerQueue();
-	~TimerQueue();
-	
+
+    class Timer
+    {
+    public:
+        Timer(Timestamp stamp, IRun* pRun, double interval)
+          :_stamp(stamp)
+          ,_id(stamp)
+          ,_pRun(pRun)
+          ,_interval(interval)        
+          {}
+        Timestamp getStamp()
+        {
+            return _stamp;
+        }
+
+        Timestamp getId()
+        {
+            return _id;
+        }
+
+        void run()
+        {
+          _pRun->run(this);
+        }
+
+        Timestamp isRepeat()
+        {
+            return _interval > 0.0;
+        }
+
+        void moveToNext()
+        {
+            _stamp = Timestamp::nowAfter(_interval);
+        }
+    private:
+        Timestamp _stamp;
+        Timestamp _id;
+        IRun* _pRun;
+        double _interval;    
+    };
+  
+    class AddTimerWrapper : public IRun
+    {
+        public:
+            AddTimerWrapper(TimerQueue* pQueue)
+                :_pQueue(pQueue)
+                {}
+            virtual void run(void* param)
+            {
+                _pQueue->doAddTimer(param);
+            }
+        private:
+            TimerQueue* _pQueue;
+    };
+
+    class CancelTimerWrapper : public IRun
+    {
+        public:
+            CancelTimerWrapper(TimerQueue* pQueue)
+                :_pQueue(pQueue)
+                {}
+
+                virtual void run(void* param)
+                {
+                    _pQueue->doCancelTimer(param);
+                }
+          private:
+              TimerQueue* _pQueue;
+
+    };
+
+    TimerQueue(EventLoop* pLoop);
+    ~TimerQueue();
+    void doAddTimer(void* param);
+    void doCancelTimer(void* param);
+
+private:
+    typedef std::pair<Timestamp, Timer*> Entry;
+    typedef std::set<Entry> TimerList;
+
+    int createTimerId();
+    vector<TimerQueue::Entry> getExpired(Timestamp now);
+    void resetTimerfd(int timerfd, Timestamp stamp);
+    bool insert(Timer* pItem);
+
+    int _timerfd;
+    TimerList _timers;
+    EventLoop* _pLoop;
+    Channel* _timerfdChannel;
+    AddTimerWrapper* _addTimerWrapper;
+    CancelTimerWrapper* _cancelTimerWrapper;
 };
 
 #endif
